1f88dcd87f40b4854961c3d159e840b94ed84c7c
vidklopcic <klopcic.vid@gmail.com>
2015-04-19 16:55:03 +0200

    uspesno se poveze na wifi

--- app/src/main/java/com/ddns/citisense/citisense_vesna/MainActivity.java
+++ app/src/main/java/com/ddns/citisense/citisense_vesna/MainActivity.java
@@ -1,25 +1,54 @@
 package com.ddns.citisense.citisense_vesna;
-
-import android.app.ActionBar;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-    private WifiManager wifiManager;
+    WifiManager mainWifi;
+    WifiReceiver receiverWifi;
+    List<ScanResult> wifiList;
+    StringBuilder sb = new StringBuilder();
+    ListView wifiListView;
+    List<String> refinedWifiList = new ArrayList<String>();
+    String vesnaPass = "passphrase";
+    ArrayAdapter<String> wifiListAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wifiManager = new WifiManager();
-        Log.d("wifi", )
+        wifiListView = (ListView) findViewById(R.id.vesna_wifi_list);
+        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        receiverWifi = new WifiReceiver();
+        registerReceiver(receiverWifi, new IntentFilter(
+                WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiListAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.list_element,
+                R.id.text,
+                refinedWifiList
+        );
+        wifiListView.setAdapter(wifiListAdapter);
+        mainWifi.startScan();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,5 +70,38 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onPause() {
+        unregisterReceiver(receiverWifi);
+        super.onPause();
+    }
+
+    public void wifi_element_clicked(View view) {
+        String ssid = ((TextView)((ViewGroup)view).getChildAt(1)).getText().toString();
+        WifiConfiguration conf = new WifiConfiguration();
+        conf.SSID = "\"" + ssid + "\"";
+        conf.preSharedKey = "\""+ vesnaPass +"\"";
+        mainWifi.addNetwork(conf);
+        List<WifiConfiguration> list = mainWifi.getConfiguredNetworks();
+        for( WifiConfiguration i : list ) {
+            if(i.SSID != null && i.SSID.equals("\"" + ssid + "\"")) {
+                mainWifi.disconnect();
+                mainWifi.enableNetwork(i.networkId, true);
+                mainWifi.reconnect();
+                break;
+            }
+        }
+    }
+
+    class WifiReceiver extends BroadcastReceiver {
+        public void onReceive(Context c, Intent intent) {
+            wifiList = mainWifi.getScanResults();
+            refinedWifiList.clear();
+            for(int i = 0; i < wifiList.size(); i++){
+                refinedWifiList.add(wifiList.get(i).SSID);
+                wifiListAdapter.notifyDataSetChanged();
+            }
+        }
     }
 }
